fc14f4596de736246b2f29fdeb0db63b2271a0c2
diegozam-dev <diegozam-dev@users.noreply.github.com>
2025-06-05 14:31:04 -0500

    add link_btn

---
+++ lib/components/buttons/link_btn.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LinkBtn extends StatefulWidget {
+  final String label;
+  // final Icon?
+
+  const LinkBtn({super.key, required this.label});
+
+  @override
+  State<LinkBtn> createState() => _LinkBtnState();
+}
+
+class _LinkBtnState extends State<LinkBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
