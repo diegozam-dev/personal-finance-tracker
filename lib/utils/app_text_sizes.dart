9f117c68af8f24b968413cdff411b58f53dc2eb4
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-06-04 19:10:50 -0500

    add the text sizes

--- lib/utils/app_text_sizes.dart
+++ lib/utils/app_text_sizes.dart
@@ -1,3 +1,9 @@
 class AppTextSizes {
-  static const double txt16 = 16;
+  static const double xl3 = 28;
+  static const double xl2 = 24;
+  static const double xl = 20;
+  static const double large = 18;
+  static const double medium = 16;
+  static const double small = 14;
+  static const double xs = 12;
 }
