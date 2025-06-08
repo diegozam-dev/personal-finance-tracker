2f0a18850fc5da2ae92c1d58cdd42c8454533315
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-06-08 07:08:13 -0500

    add colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,11 +2,42 @@ import 'dart:ui';
 
 class AppColors {
   // Basic
-  static const Color white = Color(0xFFFCFCFC);
-  static const Color black = Color(0xFF171717);
+  static const Color white = gray1;
+  static const Color black = gray12;
 
   // App
-  static const Color primary = Color(0xFF299764);
+  static const Color primary = green10;
+
+  static const Color gray1 = Color(0xFFFCFCFC);
+  static const Color gray2 = Color(0xFFF8F8F8);
+  static const Color gray3 = Color(0xFFF3F3F3);
+  static const Color gray4 = Color(0xFFEDEDED);
+  static const Color gray6 = Color(0xFFE2E2E2);
+  static const Color gray8 = Color(0xFFC7C7C7);
+  static const Color gray9 = Color(0xFF8F8F8F);
+  static const Color gray10 = Color(0xFF858585);
+  static const Color gray11 = Color(0xFF6F6F6F);
+  static const Color gray12 = Color(0xFF171717);
+
+  static const Color green1 = Color(0xFFFBFEFC);
+  static const Color green4 = Color(0xFFDDF3E4);
+  static const Color green5 = Color(0xFFCCEBD7);
+  static const Color green6 = Color(0xFFB4DFC4);
+  static const Color green7 = Color(0xFF92CEAC);
+  static const Color green8 = Color(0xFF65BA75);
+  static const Color green9 = Color(0xFF30A46C);
+  static const Color green10 = Color(0xFF299764);
+  static const Color green11 = Color(0xFF18794E);
+  static const Color green12 = Color(0xFF153226);
+
+  // Accent
+  static const accentGreen = green9;
+  static const accentRed = Color(0xFFE54D2E);
+  static const accentBlue = Color(0xFF3E63DD);
+  static const accentYellow = Color(0xFFF7CE00);
+  static const accentPurple = Color(0xFF9C2BAD);
+  static const accentOrange = Color(0xFFED5F00);
+  static const accentViolet = Color(0xFFE03177);
 
   // Text
   static const Color textPrimary = black;
