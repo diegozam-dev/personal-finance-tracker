4c2d3a4e9faa1cb02a846d13e27c1f269a2185dd
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-06-29 04:57:14 -0500

    increase icon and text sizes

--- lib/utils/app_icon_sizes.dart
+++ lib/utils/app_icon_sizes.dart
@@ -1,5 +1,5 @@
 class AppIconSizes {
-  static const double small = 16;
-  static const double medium = 20;
-  static const double large = 24;
+  static const double small = 20;
+  static const double medium = 24;
+  static const double large = 28;
 }
