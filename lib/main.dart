561bbf58c8f8d290ae202d4572acab80e00c037e
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2025-06-29 07:06:14 -0500

    test ItemListWithSeparator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:caply/components/buttons/icon_btn.dart';
 import 'package:caply/components/buttons/primary_button.dart';
 import 'package:caply/components/buttons/see_all_btn.dart';
+import 'package:caply/components/containers/item_list_with_separator.dart';
 import 'package:caply/components/headings/h1.dart';
 import 'package:caply/components/headings/h2.dart';
 import 'package:caply/components/headings/h3.dart';
@@ -8,7 +9,12 @@ import 'package:caply/components/transaction/financial_transaction.dart';
 import 'package:caply/components/transaction/goal_transaction.dart';
 import 'package:caply/models/category_model.dart';
 import 'package:caply/models/goal_model.dart';
+import 'package:caply/utils/app_accent_colors.dart';
+import 'package:caply/utils/category_types.dart';
+import 'package:caply/utils/goal_states.dart';
+import 'package:caply/utils/transaction_types.dart';
 import 'package:flutter/material.dart';
+import 'package:caply/utils/app_icons.dart';
 
 void main() {
   runApp(const MainApp());
@@ -19,7 +25,51 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int iconId = Icons.access_alarm.codePoint;
+    List<Widget> items = [
+      FinancialTransaction(
+        id: 1,
+        type: TransactionTypes.expense,
+        amount: 100.0,
+        category: CategoryModel(
+          categoryId: 1,
+          name: 'Food',
+          icon: appIcons[0]['name'],
+          color: appAccentColors[0]['name'],
+          type: CategoryTypes.expense,
+        ),
+        description: 'Lunch at the restaurant',
+        date: DateTime.now().subtract(const Duration(days: 1)),
+      ),
+      FinancialTransaction(
+        id: 2,
+        type: TransactionTypes.income,
+        amount: 200.0,
+        category: CategoryModel(
+          categoryId: 2,
+          name: 'Salary',
+          icon: appIcons[1]['name'],
+          color: appAccentColors[1]['name'],
+          type: CategoryTypes.expense,
+        ),
+        description: 'Monthly salary',
+        date: DateTime.now().subtract(const Duration(days: 7)),
+      ),
+      GoalTransaction(
+        id: 1,
+        amount: 50.0,
+        goal: GoalModel(
+          goalId: 1,
+          userId: 1,
+          name: 'Vacation',
+          goalAmount: 1000.0,
+          savedAmount: 500.0,
+          imageUrl:
+              'https://cdn.prod.website-files.com/654a7a21d047b4f79110837e/6597569753aab5ce1cb322d4_south-beach-miami-florida_HERO_FLBEACH0123-97d4bbd632a7430ea1ac08b30e6ed5d8.jpeg',
+          state: GoalStates.inProgress,
+        ),
+        date: DateTime.now().subtract(const Duration(days: 40)),
+      ),
+    ];
 
     return MaterialApp(
       home: Scaffold(
@@ -27,38 +77,9 @@ class MainApp extends StatelessWidget {
           child: Column(
             children: [
               Spacer(),
+
               SizedBox(height: 16),
-              FinancialTransaction(
-                id: 5,
-                amount: 500,
-                type: 'Income',
-                description: 'Pago por servicios prestados',
-                date: DateTime.now(),
-                category: CategoryModel(
-                  categoryId: 2,
-                  type: 'Income',
-                  name: 'Tecnópolis',
-                  icon: '',
-                  color: '',
-                ),
-              ),
-              SizedBox(height: 16),
-              GoalTransaction(
-                id: 5,
-                amount: 50,
-                type: 'Goal',
-                date: DateTime.now(),
-                goal: GoalModel(
-                  goalId: 1,
-                  userId: 1,
-                  name: 'PC Gamer',
-                  goalAmount: 800,
-                  savedAmount: 200,
-                  state: 'in progress',
-                  imageUrl:
-                      'https://www.bestcell.com.ec/img/cotiza-pc-gamer-oficina.webp',
-                ),
-              ),
+              Expanded(child: ItemListWithSeparator(items: items)),
               Spacer(),
             ],
           ),
